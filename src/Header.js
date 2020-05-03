5cbeec8347cb83394039f449db0aff824345ee2c
surendra <surendra100598@gmail.com>
2020-05-03 12:25:28 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header(){
+    return (
+        <header>
+         <img className="center" src = "https://appelse.com/wp-content/uploads/2020/01/Best-10-meme-maker-app-for-iphone-iOS-and-Android.jpg" alt="centered image" />
+
+        </header>
+    )
+}
+
+export default Header
